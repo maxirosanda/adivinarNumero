a5623184bf1a6a6dfece9b614337d7a567503999
maxirosanda <maxi_rosanda@hotmail.com>
2023-11-05 21:34:21 -0300

    clase 7 falta cambiando entre pantallas

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native'
+import Header from './components/Header'
+import StartGameScreen from './pages/StartGameScreen'
 
-export default function App() {
+export default App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <Header title={"Header"}/>
+        <StartGameScreen/>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  }
+  
+})
---
+++ pages/StartGameScreen.jsx
@@ -0,0 +1,80 @@
+import { useState } from "react"
+import {View,Text,StyleSheet,Button,TouchableWithoutFeedback, Keyboard} from "react-native"
+import Card from "../components/Card"
+import Colors from "../constants/colors"
+import Input from "../components/Input"
+import NumberContainer from "../components/NumberContainer"
+
+
+export default StartGameScreen = () => {
+
+    const [enteredValue,setEnteredValue] = useState("")
+    const [confirmed,setConfirmed] = useState(false)
+    const [selectedNumber,setSelectedNumber] = useState("")
+    const handlerInputNumber = text => setEnteredValue(text.replace(/[^0-9]/g,""))
+    const handlerResetInput = () =>  setEnteredValue("")
+    const handlerConfirmInput = () => {
+        const choseNumber = parseInt(enteredValue)
+        if(isNaN(choseNumber) || choseNumber <= 0 || choseNumber > 99 ) return
+        setConfirmed(true)
+        setSelectedNumber(choseNumber)
+        setEnteredValue("")
+    }
+   
+    return  <TouchableWithoutFeedback onPress={()=> Keyboard.dismiss()}>
+                <View style={styles.screen}>
+                    <Text style={styles.title}>Comenzar Juego</Text>
+                    <Card style={styles.inputContainer}>
+                        <Input
+                            blurOnSubmit
+                            autoCapitalization="none"
+                            autoCorrect={false}
+                            keyboardType="numeric"
+                            maxLength={2}
+                            caretHidden={true}
+                            selectionColor="blue"
+                            onChangeText = {handlerInputNumber}
+                            value={enteredValue}
+
+                        />
+                        <View style={styles.buttonContainer}>
+                        <View style={styles.button}><Button  title="Confirmar" color={Colors.primary} onPress={handlerConfirmInput}/></View>  
+                        <View style={styles.button}><Button  title="Limpiar" color={Colors.accent} onPress={handlerResetInput}/></View>  
+                        </View>
+                    </Card>
+                    {   
+                    confirmed && (
+                            <Card style={styles.summaryContainer}>
+                                <Text>Tu seleccion</Text>
+                                <NumberContainer>{selectedNumber}</NumberContainer>
+                                <Button title="Empezar juego"></Button>
+                            </Card>
+                        )
+                    }
+                </View>
+            </TouchableWithoutFeedback>    
+}
+
+const styles = StyleSheet.create({
+    screen:{
+        padding:10,
+        flex:1,
+        alignItems:"center"
+    },
+    title:{
+        fontSize:20
+    },
+    inputContainer:{
+        width:300,
+        maxWidth:"80%",
+        padding:20
+    },
+    buttonContainer:{
+        flexDirection:"row",
+        justifyContent:"space-around"
+    },
+    button:{
+        width:100
+    }
+
+})
